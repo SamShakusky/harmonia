a22363fd2269c82d33b6e1d200e6e8f8e7389eac
Shakusky <e.visloukhov@gmail.com>
2017-11-29 13:40:16 +0300

    added devider and results amount features

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
 "use strict";
 
-(function harmonise() {
+var harmonise = (function() {
 	//cache DOM
 	var $main = $('main'),
 		$input = $main.find('.input'),
 		$proportion = $main.find('.proportion'),
+		$devider = $main.find('.devider'),
 		$round = $main.find('.round'),
+		$count = $main.find('.count'),
 		$maxCol = $main.find('.max ul'),
 		$minCol = $main.find('.min ul');
 	
 	//bind events
 	$input.on('keyup', count);
+	$count.on('keyup', count);
 	
 	function count(e) {
 		if (e.keyCode == 13) {
-			let value = $(this).val(),
-				max = value,
-				min = value;
+			let max = $input.val(),
+				min = $input.val();
 			
 			$maxCol.text('');
 			$minCol.text('');
 			
-			for(let i=0; i<7;i++){
+			for(let i=0; i< $count.val(); i++){
 				max *= $proportion.val();
-				min *= $proportion.val() - 1;
-				$maxCol.append('<li title="Копировать в буфер обмена">'+Math.round(max * $round.val()) / $round.val()+'</li>');
-				$minCol.append('<li title="Копировать в буфер обмена">'+Math.round(min * $round.val()) / $round.val()+'</li>');
+				min *= 1/$proportion.val();
+				$maxCol.append('<li title="Копировать в буфер обмена">'+max.toFixed($round.val())+'</li>');
+				$minCol.append('<li title="Копировать в буфер обмена">'+min.toFixed($round.val())+'</li>');
 			}
+	  		
+			if($devider.val() == "comma") changeDevider();
 		}
+	}
+  
+	function changeDevider() {
+		$main.find('.h-content ul li').each(function(){
+			let changed = $(this).text().replace('.', ',');
+			$(this).text(changed);
+		})
+	}
+	
+	return {
+		count: count
 	}
 })();
