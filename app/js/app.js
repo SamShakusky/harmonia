56d4813be879a9f58848d964eb408b08c2e6165c
Shakusky <e.visloukhov@gmail.com>
2017-12-08 18:21:09 +0300

    changed initial html/css

--- app/js/app.js
+++ app/js/app.js
@@ -8,12 +8,14 @@ var harmonise = (function() {
 		$devider = $main.find('.devider'),
 		$round = $main.find('.round'),
 		$count = $main.find('.count'),
-		$maxCol = $main.find('.max ul'),
-		$minCol = $main.find('.min ul');
+		$maxCol = $main.find('.max'),
+		$minCol = $main.find('.min'),
+		$selectorBtn = $main.find('.selector button');
 	
 	//bind events
 	$input.on('keyup', count);
 	$count.on('keyup', count);
+	$selectorBtn.on('click', selectActive);
 	
 	function count(e) {
 		if (e.keyCode == 13) {
@@ -23,11 +25,11 @@ var harmonise = (function() {
 			$maxCol.text('');
 			$minCol.text('');
 			
-			for(let i=0; i< $count.val(); i++){
-				max *= $proportion.val();
-				min *= 1/$proportion.val();
-				$maxCol.append('<li title="Копировать в буфер обмена">'+max.toFixed($round.val())+'</li>');
-				$minCol.append('<li title="Копировать в буфер обмена">'+min.toFixed($round.val())+'</li>');
+			for(let i=0; i< 12/*$count.val()*/; i++){
+				max *= $proportion.children('.active').data('value');
+				min *= 1/$proportion.children('.active').data('value');
+				$maxCol.append('<li title="Копировать в буфер обмена">'+max.toFixed($round.children('.active').data('value'))+'</li>');
+				$minCol.append('<li title="Копировать в буфер обмена">'+min.toFixed($round.children('.active').data('value'))+'</li>');
 			}
 	  		
 			if($devider.val() == "comma") changeDevider();
@@ -39,6 +41,18 @@ var harmonise = (function() {
 			let changed = $(this).text().replace('.', ',');
 			$(this).text(changed);
 		})
+	}
+	
+	function selectActive() {
+		$(this).siblings('.active').removeClass('active');
+		$(this).addClass('active');
+		
+		let ePosition = $(this).position().left - $(this).parent().position().left,
+			eWidth = $(this).innerWidth();
+		
+		$(this).parent().find('.marker')
+			.css('transform','translateX('+ ePosition +'px)')
+			.css('width',eWidth);
 	}
 	
 	return {
